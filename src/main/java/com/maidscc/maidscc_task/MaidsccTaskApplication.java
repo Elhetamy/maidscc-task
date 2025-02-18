9d2adc9f5561f560cc1058bd2d4e89f0d614cc7b
aelhetamy <aelhetamy@kn-it.com>
2025-02-19 00:51:19 +0200

    Add Redis configuration and Refactor

--- src/main/java/com/maidscc/maidscc_task/MaidsccTaskApplication.java
+++ src/main/java/com/maidscc/maidscc_task/MaidsccTaskApplication.java
@@ -2,8 +2,10 @@ package com.maidscc.maidscc_task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MaidsccTaskApplication {
 
 	public static void main(String[] args) {
